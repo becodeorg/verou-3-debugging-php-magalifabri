9948126736482edefbbe559861188283b0aea423
Magali Fabri <magali_90@live.nl>
2022-02-15 13:19:20 +0100

    Complete ex9

--- expert.php
+++ expert.php
@@ -159,23 +159,24 @@ echo isLinkValid('http://google.com');
 //VALID link
 echo isLinkValid('http://google.com/test.txt');
 
-echo strpos('https://google.com', 'https:');
+
 
 
 // === Exercise 9 ===
-// new_exercise(9);
+new_exercise(9);
 
-// //Filter the array $areTheseFruits to only contain valid fruits
-// //do not change the arrays itself
-// $areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
-// $validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
-// //from here on you can change the code
-// for($i=0; $i <= count($areTheseFruits); $i++) {
-//     if(!in_array($areTheseFruits[$i], $validFruits)) {
-//         unset($areTheseFruits[$i]);
-//     }
-// }
-// var_dump($areTheseFruits);//do not change this
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+$areTheseFruitsLength = count($areTheseFruits);
+for ($i = 0; $i < $areTheseFruitsLength; $i++) {
+    if (!in_array($areTheseFruits[$i], $validFruits)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits); //do not change this
 
 
 
